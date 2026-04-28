784a18ab2ce45c93410754937f84b35e377cb357
eyayq <265351830+eyayq@users.noreply.github.com>
2026-04-28 21:46:09 +0700

    Update haodao.php

--- a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/haodao.php
+++ a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/haodao.php
@@ -1,7 +1,7 @@
 #EXTM3U
 #EXTINF:-1 tvg-logo="https://viiip.kitashinsaku.com//0.php" group-title="BUNGHAT VSERFREE 2 LIVE", [20:00 28/04] phát trực tiếp,xem bóng đá trực tuyến miễn phí trực tiếp bóng đá hôm nay xem bóng đá C1 trực tiếp HD
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Mobile Safari/537.36
-#EXTVLCOPT:http-referrer=https://bongtv.com.vn/live/
-#EXTVLCOPT:http-origin=https://bongtv.com.vn/live
+#EXTVLCOPT:http-referrer=https://bongtv.com.vn/
+#EXTVLCOPT:http-origin=https://bongtv.com.vn
 http://live.bongtv.com/live/sd-6MuTN5vdE1mKBTnk92.m3u8
 
